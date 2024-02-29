ff29f3d7466123204fd22a79c67d165bf6b23e84
Jean-Philippe BOUBOU <boubou.jeanphilippe@gmail.com>
2024-02-29 18:49:38 +0100

    Correct auth0 and securize route

--- BattleShip.API/Program.cs
+++ BattleShip.API/Program.cs
@@ -3,26 +3,40 @@ using System.Text;
 using System.Diagnostics;
 using BattleShip.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<BoardService>();
-builder.Services.AddSingleton<ActionService>();
-builder.Services.AddSingleton<GameService>();
-builder.Services.AddScoped<LeaderboardService>();
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddCors()
+    ;
+builder.Services
+    .AddSingleton<BoardService>()
+    .AddSingleton<ActionService>()
+    .AddSingleton<GameService>()
+    .AddScoped<LeaderboardService>()
+    ;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=leaderboard.db"));
-builder.Services.AddCors(option => 
-{
-    option.AddDefaultPolicy(builder => 
-    {
-        builder.WithOrigins("http://localhost:5249").AllowAnyHeader().AllowAnyMethod();
+
+builder.Services.AddAuthentication((options) => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c => {
+        c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+        c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidAudience = builder.Configuration["Auth0:Audience"],
+            ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}"
+        };
     });
-});
+
+builder.Services.AddAuthorization(options =>{});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,135 +45,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors();
+
+// Utilise le middleware Auth0
+
+app.UseCors(c => {
+    c.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/GetGames", () => {
-    var gameService = app.Services.GetRequiredService<GameService>();
-    var parties = new List<Guid> ();
-    foreach (var partie in gameService.parties)
-    {
-        parties.Add(partie.Id);
-    }
-    return parties;
-}).WithName("GetGames")
-  .WithDescription("Get the current game of battleship")
-  .WithOpenApi();
-app.MapGet("/StartGame", () =>
-{
-    var gameService = app.Services.GetRequiredService<GameService>();
-    var actionService = app.Services.GetRequiredService<ActionService>();
-    var currentPartie = gameService.InitGame();
-    gameService.parties.Add(currentPartie);
-    BoardService.PlaceShips(currentPartie.Player1Board, BoardService.GetAllShips());
-    BoardService.PlaceShips(currentPartie.Player2Board, BoardService.GetAllShips());
-    currentPartie.Player1StartingBoard = (char[,])currentPartie.Player1Board.Clone();
-    currentPartie.Player2StartingBoard = (char[,])currentPartie.Player2Board.Clone();
-    var positionsPlayer1 = BoardService.ListShipPositions(currentPartie.Player1Board);
-    actionService.moves = ActionService.GenerateAllMoves();
-    var boards = new CreateGameDTO(currentPartie.Id, positionsPlayer1);
-    return boards;
-}).WithName("StartGame")
-  .WithDescription("Start a new game of battleship")
-  .WithOpenApi();
-
-app.MapGet("Boards/{gameId}", (Guid gameId) =>
-{
-    var gameService = app.Services.GetRequiredService<GameService>();
-    var game = gameService.parties.FirstOrDefault(p => p.Id == gameId);
-    if (game == null)
-    {
-        return Results.NotFound("Game not found");
-    }
-    var boards = new
-    {
-        Player1Board = BoardService.ListShipPositions(game.Player1Board),
-        Player2Board = BoardService.ListShipPositions(game.Player2Board)
-    };
-
-    return Results.Ok(boards);
-})
-.WithName("GetBoards")
-.WithDescription("Get the two players boards")
-.WithOpenApi();
-app.MapGet("Leaderboard", (IServiceProvider serviceProvider) =>
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var scopedServiceProvider = scope.ServiceProvider;
-        var leaderboardService = scopedServiceProvider.GetRequiredService<LeaderboardService>();
-        var top10 = leaderboardService.GetTop10Users();
-        return Results.Ok(top10);
-    }
-}).WithName("Leaderboard")
-.WithDescription("Get the top 10 players of the leaderboard")
-.WithOpenApi();
-app.MapGet("Attack/{gameId}/{x}/{y}", (Guid gameId, int x, int y) =>
-{
-    using (var scope = app.Services.CreateScope())
-    {
-        var scopedServiceProvider = scope.ServiceProvider;
-        var leaderBoardService = scopedServiceProvider.GetRequiredService<LeaderboardService>();
-        
-        var gameService = app.Services.GetRequiredService<GameService>();
-        var actionService = app.Services.GetRequiredService<ActionService>();
-        var game = gameService.parties.FirstOrDefault(p => p.Id == gameId);
-        if (game == null)
-        {
-            return Results.NotFound("Game not found");
-        }
-        if (game.isFinished)
-        {
-            return Results.BadRequest("Game is finished");
-        }
-        var playerBoard = game.Player2Board;
-        if (playerBoard[y, x] == 'O' || playerBoard[y, x] == 'X')
-        {
-            return Results.BadRequest("Position already attacked");
-        }
-        var listAiAttack = new List<AttackDTO>();
-        var attack = actionService.Attack(game.Player1, playerBoard, x, y);
-        var opponentBoard = BoardService.ListShipPositions(game.Player2StartingBoard);
-        if (attack.sunkunBoat != "")
-        {
-            var shipLetter = attack.sunkunBoat;
-            if (opponentBoard.ContainsKey(shipLetter))
-            {
-                var positions = opponentBoard[shipLetter];
-                var positionsString = string.Join(",", positions);
-                attack.sunkunBoat = $"{shipLetter}:{positionsString}";
-            }
-        }
-
-
-        if (attack.AttackState != "Hit")
-        {
-            var aiAttack = actionService.AiAttack(game.Player2, game.Player1Board);
-            listAiAttack.Add(new AttackDTO(aiAttack.GameStatus, aiAttack.AttackState, aiAttack.MoveLabel, aiAttack.sunkunBoat));
-            while( aiAttack.AttackState == "Hit"){
-                if (aiAttack.GameStatus != "")
-                {
-                    game.Winner = game.Player2;
-                    gameService.endGame(gameId);
-                    break;
-                }
-                aiAttack = actionService.AiAttack(game.Player2, game.Player1Board);
-                listAiAttack.Add(new AttackDTO(aiAttack.GameStatus, aiAttack.AttackState, aiAttack.MoveLabel, aiAttack.sunkunBoat));
-            }
-        }
-        if (attack.GameStatus == "Win")
-        {   
-            game.Winner = game.Player1;
-            leaderBoardService.AddOrUpdateUser(game.Player1, 100);
-            gameService.endGame(gameId);
-        }
-        var attackDTO = new AttackDTO(attack.GameStatus, attack.AttackState, attack.MoveLabel, attack.sunkunBoat);
-        var result = new listAttackDTO(attackDTO, listAiAttack, game.Winner);
-        return Results.Ok(result);
-    }
-})
-.WithName("GetAttack")
-.WithDescription("Get the attack from the turn of player 1 and the AI response")
-.WithOpenApi();
+BattleShip.API.Route.RegisterRoutes(app);
 
 app.Run();
---
+++ BattleShip.API/src/Route.cs
@@ -0,0 +1,150 @@
+using BattleShip.Models;
+
+namespace BattleShip.API
+{
+    public class Route
+    {
+
+        public static void RegisterRoutes(WebApplication app)
+        {
+
+            app.MapGet("/GetGames", () =>
+            {
+                var gameService = app.Services.GetRequiredService<GameService>();
+                var parties = new List<Guid>();
+                foreach (var partie in gameService.parties)
+                {
+                    parties.Add(partie.Id);
+                }
+                return parties;
+            }).WithName("GetGames")
+            .RequireAuthorization()
+            .WithDescription("Get the current game of battleship")
+            .WithOpenApi();
+            app.MapGet("/StartGame", (HttpContext httpContext) =>
+            {
+                var name = httpContext.User.Claims.FirstOrDefault(c => c.Type == "name")?.Value;
+                var gameService = app.Services.GetRequiredService<GameService>();
+                var actionService = app.Services.GetRequiredService<ActionService>();
+                var currentPartie = gameService.InitGame(name);
+                gameService.parties.Add(currentPartie);
+                BoardService.PlaceShips(currentPartie.Player1Board, BoardService.GetAllShips());
+                BoardService.PlaceShips(currentPartie.Player2Board, BoardService.GetAllShips());
+                currentPartie.Player1StartingBoard = (char[,])currentPartie.Player1Board.Clone();
+                currentPartie.Player2StartingBoard = (char[,])currentPartie.Player2Board.Clone();
+                var positionsPlayer1 = BoardService.ListShipPositions(currentPartie.Player1Board);
+                actionService.moves = ActionService.GenerateAllMoves();
+                var boards = new CreateGameDTO(currentPartie.Id, positionsPlayer1);
+                return boards;
+            }).WithName("StartGame")
+              .RequireAuthorization()
+              .WithDescription("Start a new game of battleship")
+              .WithOpenApi();
+
+            app.MapGet("Boards/{gameId}", (Guid gameId) =>
+            {
+                var gameService = app.Services.GetRequiredService<GameService>();
+                var game = gameService.parties.FirstOrDefault(p => p.Id == gameId);
+                if (game == null)
+                {
+                    return Results.NotFound("Game not found");
+                }
+                var boards = new
+                {
+                    Player1Board = BoardService.ListShipPositions(game.Player1Board),
+                    Player2Board = BoardService.ListShipPositions(game.Player2Board)
+                };
+
+                return Results.Ok(boards);
+            })
+            .WithName("GetBoards")
+            .RequireAuthorization()
+            .WithDescription("Get the two players boards")
+            .WithOpenApi();
+            app.MapGet("Leaderboard", (IServiceProvider serviceProvider) =>
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var scopedServiceProvider = scope.ServiceProvider;
+                    var leaderboardService = scopedServiceProvider.GetRequiredService<LeaderboardService>();
+                    var top10 = leaderboardService.GetTop10Users();
+                    return Results.Ok(top10);
+                }
+            }).WithName("Leaderboard")
+            .RequireAuthorization()
+            .WithDescription("Get the top 10 players of the leaderboard")
+            .WithOpenApi();
+            app.MapGet("Attack/{gameId}/{x}/{y}", (Guid gameId, int x, int y) =>
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var scopedServiceProvider = scope.ServiceProvider;
+                    var leaderBoardService = scopedServiceProvider.GetRequiredService<LeaderboardService>();
+
+                    var gameService = app.Services.GetRequiredService<GameService>();
+                    var actionService = app.Services.GetRequiredService<ActionService>();
+                    var game = gameService.parties.FirstOrDefault(p => p.Id == gameId);
+                    if (game == null)
+                    {
+                        return Results.NotFound("Game not found");
+                    }
+                    if (game.isFinished)
+                    {
+                        return Results.BadRequest("Game is finished");
+                    }
+                    var playerBoard = game.Player2Board;
+                    if (playerBoard[y, x] == 'O' || playerBoard[y, x] == 'X')
+                    {
+                        return Results.BadRequest("Position already attacked");
+                    }
+                    var listAiAttack = new List<AttackDTO>();
+                    var attack = actionService.Attack(game.Player1, playerBoard, x, y);
+                    var opponentBoard = BoardService.ListShipPositions(game.Player2StartingBoard);
+                    if (attack.sunkunBoat != "")
+                    {
+                        var shipLetter = attack.sunkunBoat;
+                        if (opponentBoard.ContainsKey(shipLetter))
+                        {
+                            var positions = opponentBoard[shipLetter];
+                            var positionsString = string.Join(",", positions);
+                            attack.sunkunBoat = $"{shipLetter}:{positionsString}";
+                        }
+                    }
+
+
+                    if (attack.AttackState != "Hit")
+                    {
+                        var aiAttack = actionService.AiAttack(game.Player2, game.Player1Board);
+                        listAiAttack.Add(new AttackDTO(aiAttack.GameStatus, aiAttack.AttackState, aiAttack.MoveLabel, aiAttack.sunkunBoat));
+                        while (aiAttack.AttackState == "Hit")
+                        {
+                            if (aiAttack.GameStatus != "")
+                            {
+                                game.Winner = game.Player2;
+                                gameService.endGame(gameId);
+                                break;
+                            }
+                            aiAttack = actionService.AiAttack(game.Player2, game.Player1Board);
+                            listAiAttack.Add(new AttackDTO(aiAttack.GameStatus, aiAttack.AttackState, aiAttack.MoveLabel, aiAttack.sunkunBoat));
+                        }
+                    }
+                    if (attack.GameStatus == "Win")
+                    {
+                        game.Winner = game.Player1;
+                        leaderBoardService.AddOrUpdateUser(game.Player1, 100);
+                        gameService.endGame(gameId);
+                    }
+                    var attackDTO = new AttackDTO(attack.GameStatus, attack.AttackState, attack.MoveLabel, attack.sunkunBoat);
+                    var result = new listAttackDTO(attackDTO, listAiAttack, game.Winner);
+                    return Results.Ok(result);
+                }
+            })
+            .WithName("GetAttack")
+            .RequireAuthorization()
+            .WithDescription("Get the attack from the turn of player 1 and the AI response")
+            .WithOpenApi();
+
+        }
+
+    }
+}
